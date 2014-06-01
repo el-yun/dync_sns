10a6eedac025717b4e5a915a8bcea36f07e33d96
JangHun <wkdgns1234@naver.com>
2014-06-01 23:06:40 +0900

    Merge branch 'master' of https://github.com/yoonjihwan/dync_sns
    Conflicts:
    	src/dync/servlet/IssueServlet.java

--- src/dync/servlet/IssueServlet.java
+++ src/dync/servlet/IssueServlet.java
@@ -2,6 +2,7 @@ package dync.servlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -41,7 +42,7 @@ public class IssueServlet extends HttpServlet {
 
 	MultipartRequest multi;
 	String fileName = "";
-
+	private PrintWriter out;
 	private static final String ACTION_GET_ISSUE = "get_issue";
 
 	private IssuePersistentManager ipm = new IssuePersistentManager();
@@ -73,6 +74,8 @@ public class IssueServlet extends HttpServlet {
 	private void processRequest(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
+		//response.setContentType("text/html;charset=euc-kr");
+		out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "KSC5601"));
 		String action_request = request.getParameter(REQ_ACTION);
 		System.out.println(action_request);
 		if(action_request == null){
@@ -89,8 +92,9 @@ public class IssueServlet extends HttpServlet {
 			int maxSize = 5 * 1024 * 1024; // 최대 업로드 파일 크기 5MB(메가)로 제한
 			multi = new MultipartRequest(request, savePath, maxSize, "UTF-8",
 					new DefaultFileRenamePolicy());
-			
 			action_request = multi.getParameter(REQ_ACTION);
+			System.out.println("multi" + multi.getParameter(REQ_ACTION));
+			System.out.println("request"+ request.getParameter(REQ_ACTION));
 			multipart_action(action_request, request, response); // 액션 처리
 		}else{
 			request_action(action_request,request,response);
@@ -115,71 +119,98 @@ public class IssueServlet extends HttpServlet {
 			int columnValue = Integer.parseInt(request
 					.getParameter("COLUMN_VALUE"));
 			if (ipm.deleteIssue(columnName, columnValue)) {
-				gotoJsp(request, response, "/jsp/dbTest.jsp");
+				JSONObject jsonObject = new JSONObject();
+				JSONArray jsonArray = new JSONArray();
+				
+				jsonObject.put("result", "success");
+				jsonArray.add(jsonObject);
+				//request.setAttribute("RESULT", jsonArray.toString());
+				//gotoJson(request, response);
+				out.print(jsonArray.toString());
 			} else {
-				throw new ServletException("DB Query Error");
+				String errorMessage = "해당 요소가 존재하지 않아 삭제할 수 없습니다.";
+				request.setAttribute("errorMessage", errorMessage);
+				gotoJsp(request, response, "/jsp/errorPage.jsp");
+				
+				//throw new ServletException("DB Query Error");
 			}
 		} else if (action.equals(ACTION_EDIT)) {
 			System.out.println("edit 요청");
-			String issue_id = request.getParameter("ISSUE_ID");
-			Issue issue = ipm.getIssue(Integer.parseInt(issue_id));
-			request.setAttribute("issue", issue);
-			String jspPath = "/jsp/dbTest.jsp";
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher(jspPath);
-			dispatcher.forward(request, response);
-		} else if (action.equals(ACTION_UPDATE)) {
-			System.out.println("update 요청");
-			Issue issue = makeIssueBean(request);
-			if (ipm.updateIssue(issue)) {
-				gotoJsp(request, response, "/jsp/dbTest.jsp");
-			} else {
-				System.out.println("update 실패");
-
-				gotoJsp(request, response, "/jsp/errorPage.jsp");
-				// throw new ServletException("DB Query Error");
-			}
-		} else if (action.equals(ACTION_GET_ISSUE)) {
-			System.out.println("getIssue 요청");
 			int issue_id = Integer.parseInt(request.getParameter("ISSUE_ID"));
-			ArrayList<Issue> issueList = ipm.getIssueList();
-			boolean flag = true;
-			for(Issue issue : issueList){
-				if(issue.getIssue_id() == issue_id){
-					flag = false;
-					break;
-				}
-				
-			}
-			if(flag){
+			
+			if(checkIssue(issue_id)){
 				request.setAttribute("errorMessage", "유효하지 않은 ISSUE_ID");
 				gotoJsp(request, response, "/jsp/errorPage.jsp");
 				return;
 			}
 			
 			Issue issue = ipm.getIssue(issue_id);
-			JSONArray json = new JSONArray();
-			json.add(issue);
-			request.setAttribute("issueJSON", json.toString());
-			gotoJsp(request, response, "/jsp/dbTest.jsp");
-			System.out.println(json);
+			JSONArray jsonArray = new JSONArray();
+			jsonArray.add(issue);
+			
+			//request.setAttribute("RESULT", jsonArray.toString());
+			//gotoJson(request, response);
+			out.print(jsonArray.toString());
+			
+		} else if (action.equals(ACTION_UPDATE)) {
+			System.out.println("update 요청");
+			Issue issue = makeIssueBean(request);
+			if (ipm.updateIssue(issue)) {
+				
+				JSONObject jsonObject = new JSONObject();
+				JSONArray jsonArray = new JSONArray();
+				
+				jsonObject.put("result", "success");
+				jsonArray.add(jsonObject);
+				//request.setAttribute("RESULT", jsonArray.toString());
+				//gotoJson(request, response);
+				out.print(jsonArray.toString());
+			} else {
+				
+				request.setAttribute("errorMessage", "update 실패 \nISSUE_ID 또는 요소를 확인해 주세요");
+				gotoJsp(request, response, "/jsp/errorPage.jsp");
+				// throw new ServletException("DB Query Error");
+			}
+		} else if (action.equals(ACTION_GET_ISSUE)) {
+			System.out.println("getIssue 요청");
+			int issue_id = Integer.parseInt(request.getParameter("ISSUE_ID"));
+			
+			if(checkIssue(issue_id)){
+				request.setAttribute("errorMessage", "유효하지 않은 ISSUE_ID");
+				gotoJsp(request, response, "/jsp/errorPage.jsp");
+				return;
+			}
+			
+			Issue issue = ipm.getIssue(issue_id);
+			JSONArray jsonArray = new JSONArray();
+			jsonArray.add(issue);
+			//request.setAttribute("RESULT", json.toString());
+			//gotoJson(request, response);
+			
+			out.print(jsonArray.toString());
 		} else if (action.equals(ACTION_LIST)) {
 			System.out.println("list 요청");
 			ArrayList<Issue> issueList = ipm.getIssueList();
 			JSONObject json = new JSONObject();
 			JSONArray jsonArray = new JSONArray();
 			jsonArray.addAll(issueList);
-			request.setAttribute("RESULT", jsonArray.toString());
-			gotoJson(request, response);
+			//request.setAttribute("RESULT", jsonArray.toString());
+			//gotoJson(request, response);
+			out.print(jsonArray.toString());
 		}
+		out.close();
 		
 	}
 	private void multipart_action(String action, HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		if (action.equals(ACTION_INSERT)) {
-			System.out.println("insert 요청");
 			Issue issue = makeIssueBean(multi);
-			PrintWriter out = response.getWriter();
+			
+			if(!checkIssue(issue.getIssue_id())){
+				request.setAttribute("errorMessage", "ISSUE 삽입 불가 \n 중복된 ISSUE_ID");
+				gotoJsp(request, response, "/jsp/errorPage.jsp");
+				return;
+			}
 			try {
 				Enumeration files = null;
 				File file = null;
@@ -191,22 +222,33 @@ public class IssueServlet extends HttpServlet {
 					files = multi.getFileNames();
 					String name = (String) files.nextElement();
 					file = multi.getFile(name);
+					issue.setUpload("C:/DEVSNS/saveFile/" + fileName);//실제 서버 경로로 파일 저장
 				}
 			} catch (Exception e) {
 				System.out.print("예외 발생 : " + e);
 			}
+			
 			if (ipm.insertIssue(issue)) {
-				gotoJsp(request, response, "/jsp/dbTest.jsp");
+				JSONObject jsonObject = new JSONObject();
+				JSONArray jsonArray = new JSONArray();
+				
+				jsonObject.put("result", "success");
+				jsonArray.add(jsonObject);
+				//request.setAttribute("RESULT", jsonArray.toString());
+				//gotoJson(request, response);
+				
+				out.print(jsonArray.toString());
+				
+				
+				
 			} else {
-				System.out.println("insert 실패");
 				request.setAttribute("errorMessage", "유효하지 않은 USER_ID");
 				gotoJsp(request, response, "/jsp/errorPage.jsp");
-
+				return;
 				// throw new ServletException("DB Query Error");
 			}
 		} 
-		
-		
+		out.close();
 	}
 
 	private Issue makeIssueBean(HttpServletRequest request) {
@@ -282,5 +324,16 @@ public class IssueServlet extends HttpServlet {
 		RequestDispatcher dispatcher = getServletContext()
 				.getRequestDispatcher(jspPath);
 		dispatcher.forward(request, response);
+	}
+	private boolean checkIssue(int issue_id){
+		ArrayList<Issue> issueList = ipm.getIssueList();
+		boolean flag = true;
+		for(Issue issue : issueList){
+			if(issue.getIssue_id() == issue_id){
+				flag = false;
+				break;
+			}
+		}
+		return flag;
 	}
 }
